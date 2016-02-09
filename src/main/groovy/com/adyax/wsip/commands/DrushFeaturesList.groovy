a11d2d03d714ba660d39c669985e0e18196fb2bd
Alexander Tolstikov <atolstikov@adyax.com>
2016-02-09 23:16:25 +0300

    Changed drushFeaturesList command

--- src/main/groovy/com/adyax/wsip/commands/DrushFeaturesList.groovy
+++ src/main/groovy/com/adyax/wsip/commands/DrushFeaturesList.groovy
@@ -14,13 +14,17 @@ class DrushFeaturesList extends Command {
 
     def perform() {
         def features = []
-        String output = drush command: "fl --format=json", dir: new File(dir + "/docroot"), noSimulate: noSimulate
+        String output = drush command: "fl --format=json", dir: new File(dir, config.drupalDir), noSimulate: noSimulate
         output = output.substring(output.indexOf('[{'))
         def jsonOutput = new JsonSlurper().parseText(output)
         jsonOutput.each { feature ->
             if (feature['State'] == state) {
                 features << feature
             }
+        }
+        if (features.size() > 0) {
+            def result = new groovy.json.JsonBuilder([features: features])
+            fail("Features overridden: ${result.toPrettyString()}")
         }
         features
     }
